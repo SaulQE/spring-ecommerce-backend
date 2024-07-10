f3626fb26d48d6b042c5380dda2cb661f11b3403
SaulQE <saul.esquivel.028@gmail.com>
2024-07-09 19:19:42 -0500

    add dto

---
+++ microservice-product/src/main/java/com/microservice/product/controller/dto/InventoryDTO.java
@@ -0,0 +1,17 @@
+package com.microservice.product.controller.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryDTO
+{
+    private Integer quantity;
+    private Long productId;
+}
