6583994088d4165a10dd2fef195af3419ca17206
SaulQE <saul.esquivel.028@gmail.com>
2024-08-02 16:15:13 -0500

    add microservice category

---
+++ microservice-category/src/main/java/com/microservice/category/MicroserviceCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCategoryApplication.class, args);
+	}
+
+}
