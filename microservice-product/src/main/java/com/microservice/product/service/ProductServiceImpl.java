4eb0d886cdcfc5ebcaaaa6c4d65a16d0872e9a4f
SaulQE <saul.esquivel.028@gmail.com>
2024-08-05 15:45:08 -0500

    add serviceImpl ProductWithCategoryResponse

--- microservice-product/src/main/java/com/microservice/product/service/ProductServiceImpl.java
+++ microservice-product/src/main/java/com/microservice/product/service/ProductServiceImpl.java
@@ -1,9 +1,12 @@
 package com.microservice.product.service;
 
+import com.microservice.product.client.CategoryClient;
 import com.microservice.product.client.InventoryClient;
+import com.microservice.product.controller.dto.CategoryDTO;
 import com.microservice.product.controller.dto.InventoryDTO;
 import com.microservice.product.entities.Product;
 import com.microservice.product.http.response.InventoryByProductResponse;
+import com.microservice.product.http.response.ProductWithCategoryResponse;
 import com.microservice.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,9 @@ public class ProductServiceImpl implements IProductService
 
     @Autowired
     private InventoryClient inventoryClient;
+
+    @Autowired
+    private CategoryClient categoryClient;
 
     @Override
     @Transactional(readOnly = true)
@@ -66,6 +72,23 @@ public class ProductServiceImpl implements IProductService
                 .descriptionLong(product.getDescriptionLong())
                 .price(product.getPrice())
                 .inventoryDTOList(inventoryDTOList)
+                .build();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ProductWithCategoryResponse findProductWithCategory(Long productId)
+    {
+        Product product = productRepository.findById(productId).orElse(new Product());
+        CategoryDTO categoryDTO = categoryClient.getCategoryById(product.getCategoryId());
+
+        return ProductWithCategoryResponse.builder()
+                .productId(product.getProductId())
+                .name(product.getName())
+                .descriptionShort(product.getDescriptionShort())
+                .descriptionLong(product.getDescriptionLong())
+                .price(product.getPrice())
+                .categoryDTO(categoryDTO)
                 .build();
     }
 }
