8b8fc44e14bc79c19ef1ca796f94073b1f90fe0a
SaulQE <saul.esquivel.028@gmail.com>
2024-08-09 17:19:53 -0500

    add method

--- microservice-image/src/main/java/com/microservice/image/service/ImageService.java
+++ microservice-image/src/main/java/com/microservice/image/service/ImageService.java
@@ -3,9 +3,13 @@ package com.microservice.image.service;
 import com.microservice.image.entities.Image;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface ImageService
 {
     Image uploadImage(MultipartFile file) throws Exception;
     void deleteImage(Image image) throws Exception;
-    Image findByPublicId(String publicId);
+    Image updateImage(Long imageId, MultipartFile file) throws Exception;
+    Image findById(Long imageId);
+    List<Image> findAll();
 }
